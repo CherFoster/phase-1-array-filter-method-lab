67bae1944a0dc7c00184acab5ca14cd7997b6c40
Cherise Foster <cherise.foster@outlook.com>
2023-03-02 18:01:14 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+function findMatching(drivers, driver){
+    return drivers.filter((newDriver) => 
+    newDriver.toLowerCase().includes(driver.toLowerCase()))
+};
+
+function fuzzyMatch(drivers, query) {
+    return drivers.filter(function(driver){
+        return driver.slice(0, query.length) === query
+    })
+}
+
+function matchName(drivers, query) {
+    return drivers.filter(driver => driver.name === query)
+}
